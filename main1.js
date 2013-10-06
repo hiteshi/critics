8270c7ca1b219f9b753b2a95015ceea376d232ef
hiteshi <hitshines@gmail.com>
2013-10-06 18:10:09 +0530

    second commit

---
+++ main1.js
@@ -0,0 +1,57 @@
+function mOver(obj)
+{
+obj.innerHTML="mobile</br>laptops</br>games"
+}
+			   
+function mOut(obj)
+{
+obj.innerHTML="ELECTRONICS"
+}
+
+
+function mOver1(obj)
+{
+obj.innerHTML="sci-fic</br>biography</br>fiction"
+}
+
+function mOut1(obj)
+{
+obj.innerHTML="BOOKS"
+}
+
+
+function mOver2(obj)
+{
+obj.innerHTML="clothing</br>watches</br>bags"
+}
+
+function mOut2(obj)
+{
+obj.innerHTML="ACCESSORIES"
+}
+
+
+function mOver3(obj)
+{
+obj.innerHTML="furniture</br>cooking</br>living"
+}
+
+function mOut3(obj)
+{
+obj.innerHTML="HOME"
+}
+
+function myFunction()
+{
+document.getElementById("demo").innerHTML="Username:</br>Password:";
+}
+
+/*function mOver4(obj)
+{
+obj.innerHTML="<img src="water.jpg" width="1000" height="200">"
+}
+
+function mOut4(obj)
+{
+obj.innerHTML="<img src="alps.jpg" width="1000" height="200">"
+}*/
